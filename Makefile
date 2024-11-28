8bd6aec9864fc157ca8b7c10e3ce0467eb39e86e
Dima <ciba91@gmail.com>
2024-11-28 15:53:18 +0300

    first step

---
+++ Makefile
@@ -0,0 +1,4 @@
+install:
+		npm ci
+gendiff:
+		node bin/gendiff.js
---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import { Command } from 'commander/esm.mjs'
+const program = new Command();
+
+program
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filePath1, filePath2) => {
+    console.log(genDiff(filePath1, filePath2, program.opts().format));
+  });
+program.parse();
